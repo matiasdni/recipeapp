bc1961d89f0454da7a39b4cdc8de1a31e5559285
matiasdn <matiasdn@metropolia.fi>
2022-04-22 17:43:51 +0300

    basic functionality for testing purposes in the future

--- app/src/main/java/com/example/androidproject/MainActivity.java
+++ app/src/main/java/com/example/androidproject/MainActivity.java
@@ -1,14 +1,44 @@
 package com.example.androidproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    // references to components
+    RecyclerView recipesView;
+    DBHelper dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recipesView = findViewById(R.id.view_recipes);
+
+        dbHelper = new DBHelper(MainActivity.this);
+
+        ArrayList<Recipe> recipes = new ArrayList<>(dbHelper.getRecipes());
+        RecipesViewAdapter adapter = new RecipesViewAdapter(this);
+        adapter.setRecipes(recipes);
+        /* TODO:
+            retrieve data from database to recipes */
+
+
+        recipesView.setAdapter(adapter);
+        recipesView.setLayoutManager(new LinearLayoutManager(this));
+
+        // add test cases
+
+
+
     }
 }
