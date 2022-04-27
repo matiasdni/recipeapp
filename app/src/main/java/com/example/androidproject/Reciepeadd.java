168bf85319e4898c222e0df3e5a70ec392462b82
KimRautiainen <kim.rautiainen@hotmail.com>
2022-04-27 11:50:06 +0300

    New activity

---
+++ app/src/main/java/com/example/androidproject/Reciepeadd.java
@@ -0,0 +1,15 @@
+package com.example.androidproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Reciepeadd extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reciepeadd);
+    }
+
+}
